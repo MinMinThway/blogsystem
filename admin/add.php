8f7c4e6debab7648d4b49f9151cfc2e013a697ea
MinMinThway <minthway668@example.com>
2021-01-14 21:44:17 +0630

    blog first commit

---
+++ admin/add.php
@@ -0,0 +1,76 @@
+ <?php
+ 	require '../config/config.php';
+ 	session_start();
+ 	if ($_POST) {
+ 		$file='images/'.($_FILES['image']['name']);
+ 		$imageType=pathinfo($file, PATHINFO_EXTENSION);
+ 		if ($imageType!='png' && $imageType!='jpg' && $imageType!='jpeg') {
+ 			echo "<script> alert('Image must be png or jpg or jpeg') </script>";
+ 		}
+ 		else
+ 		{
+ 			$title=$_POST['title'];
+ 			$content=$_POST['content'];
+ 			$image=$_FILES['image']['name'];
+ 			move_uploaded_file($_FILES['image']['tmp_name'],$file);
+ 			$stmt=$pdo->prepare("INSERT INTO posts(title,content,author_id,image) VALUES (:title,:content,:author_id,:image)");
+
+ 			$result=$stmt->execute(
+ 				array(":title"=>$title,":content"=>$content,":image"=>$image,":author_id"=>$_SESSION['user_id'],)
+ 			);
+ 			if ($result) {
+ 				echo "<script> alert('Successfully Added');window.location.href='index.php';</script>";
+ 			}
+ 		
+ 		}
+ 	}
+
+ ?>
+ <?php 
+
+  	require 'header.html';
+
+?>
+	  <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+         <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Bordered Table</h3>
+                <a href="index.php" class="float-right btn btn-success">Back</a>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+               <form action="" method="POST" enctype='multipart/form-data'>
+               	 <div class="form-group">
+                	<label>Title</label>
+                	<input type="text" name="title" class="form-control" value="" required>
+                </div>
+                <div class="form-group">
+                	<label>Content</label>
+                	<textarea class="form-control" name="content"></textarea>
+                </div>
+                <div class="form-group">
+                	<label>Image</label>
+                	<input type="file" name="image" class="form-control-file" value="" required>
+                </div>
+                <div class="form-group">
+                	<input type="submit" name="" value="SUBMIT" class="btn btn-success">
+                </div>
+               </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+<?php 
+
+  require 'footer.html';
+
+?>
---
+++ admin/edit.php
@@ -0,0 +1,93 @@
+<?php 
+    session_start();
+    if (empty($_SESSION['user_id']) && empty($_SESSION['logged_in'])) {
+    header('Location: login.php');
+  }
+  require '../config/config.php';
+
+  if ($_POST) {
+    $id=$_POST['id'];
+    $title=$_POST['title'];
+    $content=$_POST['content'];
+    if($_FILES['image']['name'] != null) {
+      $file='images/'.($_FILES['image']['name']);
+      $imageType=pathinfo($file,PATHINFO_EXTENSION );
+        if ($imageType!='jpg' && $imageType!='png' && $imageType!='jpeg')  {
+          echo "<script> alert('Image must be jpg or jpeg or png')";
+        }
+        else
+        {
+          $image=$_FILES['image']['name'];
+          $v=move_uploaded_file($_FILES['image']['tmp_name'],$file);
+          $stmt=$pdo->prepare("UPDATE posts SET title='$title',content='$content',image='$image' WHERE id='$id'");
+          $result=$stmt->execute();
+          if ($result) {
+            echo "<script> alert('Update Successsfully');window.location.href='index.php'; </script>";
+          }
+        }
+
+    }
+    else
+    {
+      $stmt=$pdo->prepare("UPDATE posts SET title='$title',content='$content' WHERE id='$id'");
+      $result=$stmt->execute();
+      if ($result) {
+        echo "<script> alert('Update Successsfully');window.location.href='index.php'; </script>";
+      }
+    }
+  }
+   $stmt=$pdo->prepare("SELECT * FROM posts WHERE id=".$_GET['id']);
+  $stmt->execute();
+  $result=$stmt->fetchALL();
+
+
+?>
+<?php
+      require 'header.html';
+
+?>
+   <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+         <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Bordered Table</h3>
+                <a href="index.php" class="float-right btn btn-success">Back</a>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+               <form action="" method="POST" enctype='multipart/form-data'>
+                <input type="text" name="id" value="<?php echo $result[0]['id']; ?>">
+                 <div class="form-group">
+                  <label>Title</label>
+                  <input type="text" name="title" class="form-control" value="<?php echo $result[0]['title'] ?>" required>
+                </div>
+                <div class="form-group">
+                  <label>Content</label>
+                  <textarea class="form-control" name="content"><?php echo $result[0]['content']; ?></textarea>
+                </div>
+                <div class="form-group">
+                  <label>Image</label>
+                  <img src="images/<?php echo $result[0]['image'];  ?>" width="100px" height="100px" class="img-fluid">
+                  <input type="file" name="image" class="form-control-file" value="" >
+                </div>
+                <div class="form-group">
+                  <input type="submit" name="" value="UPDATE" class="btn btn-success">
+                </div>
+               </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+<?php 
+
+  require 'footer.html';
+
+?>
